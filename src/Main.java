d1960bd417bf807586636cff263eda450b7cd2d2
ViharevN <swix_off@mail.ru>
2022-10-13 20:44:19 +0300

    домашнее задание 2

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,26 @@ public class Main {
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
+        //домашнее задание -2
+        //задание 1
+        System.out.println();
+
+        for (int i = 1904; i <= 2096; i +=4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        //задание 2
+        System.out.println();
+
+        for (int i = 7; i <= 98; i+=7) {
+            System.out.print(i + " ");
+        }
+
+        //задание 3
+        System.out.println();
+
+        for (int i = 1; i <= 512; i*=2) {
+            System.out.print(i + " ");
+        }
     }
 }
