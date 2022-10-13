1eea6ded137379b1500b22746f69fee7f3b7e65e
ViharevN <swix_off@mail.ru>
2022-10-13 20:29:26 +0300

    add homeworks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задание 1
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        //задание 2
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        //задание 3
+        for (int i = 0; i <= 17; i+=2) {
+            System.out.println(i);
+        }
+        //задание 4
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
     }
 }
